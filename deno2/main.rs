4032511f0b77e18f09b62e222563656f84c1678b
Ryan Dahl <ry@tinyclouds.org>
2018-06-16 01:43:23 +0200

    First pass at deno rust

---
+++ deno2/main.rs
@@ -0,0 +1,34 @@
+extern crate libc;
+use libc::c_char;
+use libc::c_int;
+use std::ffi::CStr;
+use std::ffi::CString;
+
+#[link(name = "deno", kind = "static")]
+extern "C" {
+    fn deno_v8_version() -> *const c_char;
+    fn deno_init();
+    fn deno_set_flags(argc: *mut c_int, argv: *const *const c_char);
+}
+
+fn set_flags() {
+    // Create a vector of zero terminated c strings.
+    let argv = std::env::args()
+        .map(|arg| CString::new(arg).unwrap().as_ptr())
+        .collect::<Vec<_>>();
+    let mut argc = argv.len() as c_int;
+    unsafe {
+        // pass the pointer of the vector's internal buffer to a C function
+        deno_set_flags(&mut argc, argv.as_ptr());
+    };
+}
+
+fn main() {
+    println!("Hi");
+    set_flags();
+    unsafe { deno_init() };
+    let v = unsafe { deno_v8_version() };
+    let c_str = unsafe { CStr::from_ptr(v) };
+    let version = c_str.to_str().unwrap();
+    println!("version: {}", version);
+}
